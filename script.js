8fc288bcf9709a5559b69eac6a0543df0c156998
Aron Adams <aronrapetto@hotmail.com>
2022-03-23 21:28:14 -0300

    requisito 6 aprensentando falha

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const inputTexto = document.getElementById('text-input');
 const inputArquivo = document.getElementById('meme-insert');
+const botaoBordaFire = document.getElementById('fire');
+const botaoBordaWater = document.getElementById('water');
+const botaoBordaEarth = document.getElementById('earth');
 
 function adicionaTexto() {
   const textoImagem = document.getElementById('meme-text');
   textoImagem.innerText = inputTexto.value;
 }
+
 // Solução para resolver erro do fakepath oriunda link disponibilizado no redme do projeto (https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977)
 function addImagem(event) {
   document.getElementById('meme-image').src = URL.createObjectURL(event.target.files[0]);
 }
 
+function alteraBorda(event) {
+  document.getElementById('meme-image-container').style.border = event.target.style.border;
+  console.log(document.getElementById('meme-image-container').style.border);
+  console.log(event.target.style.border);
+}
+
 inputArquivo.addEventListener('change', addImagem);
 
 inputTexto.addEventListener('keyup', adicionaTexto);
+
+botaoBordaFire.addEventListener('click', alteraBorda);
+botaoBordaWater.addEventListener('click', alteraBorda);
+botaoBordaEarth.addEventListener('click', alteraBorda);
